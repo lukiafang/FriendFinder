92644faa01d06277e48d0245e2d147e639e54e16
lukia <xhumen@msn.com>
2018-09-13 18:35:44 -0400

    init

---
+++ server.js
@@ -0,0 +1,28 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// People (DATA)
+// =============================================================
+var people = [
+    {     
+      name: "Yoda",
+      
+      score: 30,
+    },
+    
+  ];
+// Routes
+// =============================================================
+
