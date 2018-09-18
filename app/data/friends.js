738f964f6211d94600f687646832b50142aad0fa
lukia <447227675@qq.com>
2018-09-18 00:40:40 -0400

    1

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friends ={
+    name: "Lucky",
+    photo: "http://www.animamacau.org/wp-content/uploads/2015/04/Lucky.3090.jpg",
+    scores: [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "5",
+        "4",
+        "3",
+        "2",
+        "1"
+    ]
+};
+module.exports = friends;
